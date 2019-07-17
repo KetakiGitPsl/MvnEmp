8cc9f734afbe0ee7ee9633720893d47b7183704e
KetakiGitPsl <ketaki7d@gmail.com>
2019-07-17 16:41:42 +0530

    added change 7777

--- src/main/java/com/psl/Employee/service/EmployeeServiceImpl.java
+++ src/main/java/com/psl/Employee/service/EmployeeServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.psl.Employee.dao.IEmployeeDAO;
-import com.psl.Employee.entity.Employee;      
+import com.psl.Employee.entity.Employee;        
 
 @Service
 public class EmployeeServiceImpl implements IEmployeeService {
